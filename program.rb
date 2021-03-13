9634894e7d4ea9d47e70cdab3453588d2a1b8dfe
blooond <72251104+blooond@users.noreply.github.com>
2021-03-13 21:52:17 +0300

    Update program.rb

--- program.rb
+++ program.rb
@@ -1,3 +1,5 @@
+NUMBER = 26
+
 class CaesarCipher
   attr_writer :key, :text
 
@@ -14,7 +16,7 @@ class CaesarCipher
 
   def decryption
     @encrypted_string.each_char do |letter|
-      (26 - @key).times { letter = letter.next } if ('a'..'z').include?(letter.downcase)
+      (NUMBER - @key).times { letter = letter.next } if ('a'..'z').include?(letter.downcase)
       @decrypted_string << letter[-1]
     end
     @decrypted_string
